998e1ec36c6de685269d4c8ec919b8759c31ef7e
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-06 14:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+    codes.forEach((input, idx) => {
+      input.addEventListener('input', (e) => {
+        const value = e.target.value;
+        if (value) {
+          if (idx < codes.length - 1) {
+            codes[idx + 1].focus();
+          }
+        }
+      });
+
+      input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+          if (!input.value && idx > 0) {
+            codes[idx - 1].focus();
+          }
+        }
+      });
+    });
