49be8ceb95549d57d3ea12c10f72afeb643c28d4
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-06 14:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 //your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
-
+window.addEventListener('DOMContentLoaded', () => {
+  inputs[0].focus();
+});
     inputs.forEach((input, index) => {
       input.addEventListener('input', (e) => {
         const value = e.target.value;
-        if (value && index < inputs.length - 1) {
-          
-            inputs[index + 1].focus();
-          
-        }
+        if (value) {
+	      if (index < inputs.length - 1) {
+	        inputs[index + 1].focus();
+	      }
+	    }
       });
 
       input.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && !e.target.value && index > 0) {
-			inputs[index - 1].value = '';
-            inputs[index - 1].focus();
-          
+        if (e.key === 'Backspace') {
+			if (input.value === "") {
+		        if (index > 0) {
+		          inputs[index - 1].focus();
+		          inputs[index - 1].value = "";
+		        }
+		      } else {
+		        input.value = ""; 
+		      }
         }
       });
     });
-
-window.onload = () => {
-	inputs[0].focus;
-}
